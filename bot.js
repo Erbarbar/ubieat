c91f1a7784b1de04bbd3fb2db05976c508d95601
Erbarbar <filipelopez3@gmail.com>
2018-06-18 14:40:52 +0100

    Downloads image from link

--- bot.js
+++ bot.js
@@ -1,19 +1,40 @@
 //var express    = require('express');
 const bodyParser = require('body-parser');
 const Slack      = require('slack-node');
+var request = require('request');
+var fs = require('fs');
 
+
+// point bot to slack channel
+const bot_link = process.env.LINK; // no hack, pls
 const slack = new Slack();
+slack.setWebhook(bot_link); // the channel is defined on slack app settings
 
-const bot_link = process.env.LINK;
 
-slack.setWebhook(bot_link);
+
+var url = "https://scontent.fopo2-1.fna.fbcdn.net/v/t1.0-9/35464654_1759158370816852_1085733037682982912_n.jpg?_nc_cat=0&oh=a9bd7f868dc5639d416aa380a5967078&oe=5BA595FF";
+var filename = "pic.png";
+
+// download image
+downloadImage(url,filename);
+
 
 
 
 testMessage(createMessageText());
 
 
+// FUNCTIONS --------------------------------------------------
 
+// Downloads image from <input> to <output>
+function downloadImage(input, output){
+    var writeFileStream = fs.createWriteStream(output);
+    request(input).pipe(writeFileStream).on('close', function() {
+      console.log(input, 'saved to', output)
+    });
+}
+
+// sends <message> as a text message to slack 
 function testMessage(message){
     slack.webhook(
         {
