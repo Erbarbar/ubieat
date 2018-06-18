b1be75b0dcfc07e2201c07c432c75c211093e4b3
Erbarbar <filipelopez3@gmail.com>
2018-06-18 14:28:17 +0100

    Add message channel with hardcoded menu

--- bot.js
+++ bot.js
@@ -8,20 +8,50 @@ const bot_link = process.env.LINK;
 
 slack.setWebhook(bot_link);
 
-console.log("LINK = " + bot_link);
 
-slack.webhook(
-    {
-        channel: "#ubi-eat",
-        username: "test",
-        text: "Hello World!"
-    }, 
-    function(err, response) {
-        if (err === null)
-            console.log("sent message");
-        else{
-            console.log(err);
-            console.log(response);
+
+testMessage(createMessageText());
+
+
+
+function testMessage(message){
+    slack.webhook(
+        {
+            text: "Chegou a hora de escolher!",
+            attachments:
+            [
+                {   
+                    mrkdwn_in:"text",
+                    text: message,
+                    fallback: "Ups, parece que não dá para votar...",
+                    callback_id: "vote",
+                    color: "#3AA3E3"/*,
+                    uncomment to add button
+                    actions:
+                    [
+                        {   
+                            name: "button",
+                            text: "Votar",
+                            type: "button",
+                            value: "ipn"
+                        }
+                    ]*/
+                }
+
+            ]
+        }, 
+        function(err, response) {
+            if (err === null)
+                console.log("[Message sent]:\n" + message);
+            else{
+                console.log(err);
+                console.log(response);
+            }
         }
-    }
-);
+    );
+}
+
+
+function createMessageText(){
+    return "Menú IPN:\n:bowl_with_spoon: *Sopa do dia*: Canja de galinha\n:meat_on_bone: *Carne*: Lasanha de carne\n:ear_of_rice: *Vegetariano*: Lasanha de espinafres";
+}
